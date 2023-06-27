42ff884b0e07aec06f7d386461510e243158c392
Jose0863 <jaosorio807@soy.sena.edu.co>
2023-06-26 19:22:34 -0500

    se retoma proyecto

---
+++ library/configServer.php
@@ -0,0 +1,6 @@
+<?php
+define("SERVER", "dbservermysqli.mysql.database.azure.com");
+define("USER", "albeiro_admin");
+define("BD", "store");
+define("PASS", "Earv1234");
+define("SSL", "DigiCertGlobalRootCA.crt.pem");
