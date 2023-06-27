ed461f91d68394a0429036b7306c4cf3c4b53577
Jose0863 <jaosorio807@soy.sena.edu.co>
2023-06-26 20:58:06 -0500

    se retoma proyect

--- library/configServer.php
+++ library/configServer.php
@@ -1,6 +1,12 @@
 <?php
-define("SERVER", "bddreamme.mysql.database.azure.com");
-define("USER", "joseAdmin");
+//define("SERVER", "bddreamme.mysql.database.azure.com");
+//define("USER", "joseAdmin");
+//define("BD", "store");
+//define("PASS", "BdlostDream01");
+//define("SSL", "DigiCertGlobalRootCA.crt.pem");
+
+
+define("SERVER", "localhost");
+define("USER", "root");
 define("BD", "store");
-define("PASS", "BdlostDream01");
-define("SSL", "DigiCertGlobalRootCA.crt.pem");
+define("PASS", "");
